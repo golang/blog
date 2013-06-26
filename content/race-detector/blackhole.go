a74a202d61f941a785a65ae8a18c40c1af171a84
Andrew Gerrand <adg@golang.org>
2013-06-26 10:15:49 +1000

    go.blog: add Introducing the Go Race Detector
    R=r, dvyukov
    CC=golang-dev
    https://golang.org/cl/10532043

---
+++ content/race-detector/blackhole.go
@@ -0,0 +1,17 @@
+package main
+
+var blackHole [4096]byte // shared buffer
+
+func (devNull) ReadFrom(r io.Reader) (n int64, err error) {
+	readSize := 0
+	for {
+		readSize, err = r.Read(blackHole[:])
+		n += int64(readSize)
+		if err != nil {
+			if err == io.EOF {
+				return n, nil
+			}
+			return
+		}
+	}
+}
