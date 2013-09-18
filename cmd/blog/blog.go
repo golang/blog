a7251a3023bb3d78672ef1620be8de4151b3b41b
Andrew Gerrand <adg@golang.org>
2013-09-18 15:16:48 +1000

    go.blog: use godoc design, use blog package from go.tools
    Also delete go.blog/pkg/blog and unused static files.
    
    R=golang-dev, dsymonds
    https://golang.org/cl/13754043

--- cmd/blog/blog.go
+++ cmd/blog/blog.go
@@ -7,15 +7,45 @@
 package main
 
 import (
-	"code.google.com/p/go.blog/pkg/blog"
+	"net/http"
+	"strings"
+	"time"
+
+	"code.google.com/p/go.tools/godoc/blog"
+	"code.google.com/p/go.tools/godoc/static"
+
 	_ "code.google.com/p/go.tools/godoc/playground"
 )
 
 const hostname = "blog.golang.org" // default hostname for blog server
 
-var config = &blog.Config{
+var config = blog.Config{
 	Hostname:     hostname,
 	BaseURL:      "http://" + hostname,
+	GodocURL:     "http://golang.org",
 	HomeArticles: 5,  // articles to display on the home page
 	FeedArticles: 10, // articles to include in Atom and JSON feeds
+	PlayEnabled:  true,
+}
+
+func init() {
+	// Redirect "/blog/" to "/", because the menu bar link is to "/blog/"
+	// but we're serving from the root.
+	redirect := func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+	http.HandleFunc("/blog", redirect)
+	http.HandleFunc("/blog/", redirect)
+
+	http.Handle("/lib/godoc/", http.StripPrefix("/lib/godoc/", http.HandlerFunc(staticHandler)))
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Path
+	b, ok := static.Files[name]
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+	http.ServeContent(w, r, name, time.Time{}, strings.NewReader(b))
 }
