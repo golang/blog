c7a7ecad260d219c1b693f8d76238a36ba514d71
Andrew Gerrand <adg@golang.org>
2013-12-13 09:27:07 +1100

    go.blog: add "Inside the Go Playground" article
    R=r, rsc, dsymonds
    https://golang.org/cl/39180043

---
+++ content/playground/time.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	stop := time.After(3 * time.Second)
+	tick := time.NewTicker(1 * time.Second)
+	defer tick.Stop()
+	for {
+		select {
+		case <-tick.C:
+			fmt.Println(time.Now())
+		case <-stop:
+			return
+		}
+	}
+}
