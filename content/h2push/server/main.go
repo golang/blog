c36abd32f2bd6e4a9f96085bdb4e0c534e6fe5fc
Brad Fitzpatrick <bradfitz@golang.org>
2017-04-02 13:10:43 -0700

    content/h2push/server: fix pre-Go1.8 build, add copyright header
    Change-Id: Iea6d106d2b5a4f987b3f5b69799508bcc876d9c2
    Reviewed-on: https://go-review.googlesource.com/39271
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Jaana Burcu Dogan <jbd@google.com>

--- content/h2push/server/main.go
+++ content/h2push/server/main.go
@@ -1,4 +1,10 @@
-// server is a server with HTTP/2 Server Push support.
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build go1.8
+
+// The server command demonstrates a server with HTTP/2 Server Push support.
 package main
 
 import (
