7ed0a50146b4d79f8b1027b56aa37a966162f30e
Agniva De Sarker <agnivade@yahoo.co.in>
2018-12-13 14:14:49 +0530

    content/context: use req.WithContext
    (*Transport).CancelRequest is now deprecated. Use the new
    (*Request).WithContext method which inherits the parent context.
    
    Fixes golang/go#29075
    
    Change-Id: I4a275a396c6ed64eef73b688a049e7bc372f07d4
    Reviewed-on: https://go-review.googlesource.com/c/153957
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- content/context/google/google.go
+++ content/context/google/google.go
@@ -81,13 +81,11 @@ func Search(ctx context.Context, query string) (Results, error) {
 // for f to exit, and returns ctx.Err. Otherwise, httpDo returns f's error.
 func httpDo(ctx context.Context, req *http.Request, f func(*http.Response, error) error) error {
 	// Run the HTTP request in a goroutine and pass the response to f.
-	tr := &http.Transport{}
-	client := &http.Client{Transport: tr}
 	c := make(chan error, 1)
-	go func() { c <- f(client.Do(req)) }()
+	req = req.WithContext(ctx)
+	go func() { c <- f(http.DefaultClient.Do(req)) }()
 	select {
 	case <-ctx.Done():
-		tr.CancelRequest(req)
 		<-c // Wait for f to return.
 		return ctx.Err()
 	case err := <-c:
